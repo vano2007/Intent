dc64f1d775aa52e0f2b718309e76977f887b80b0
Eduard Angelskiy <ed_angel74@mail.ru>
2023-04-13 18:06:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    // поле кнопки
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // привязка id к полю
+        button = findViewById(R.id.button);
+
+        // обработка нажатия кнопки
+        button.setOnClickListener(listener);
+    }
+    // создание слушателя
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // переключение на другой экран
+            Intent intent = new Intent(getApplicationContext(), MainActivity2.class); // создание намерения переключения на другой экран
+            startActivity(intent); // запуск дополнительной активити через намерение
+        }
+    };
+}
